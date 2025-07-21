bd6c27d48817d802f6a744f43037602b310fb970
dfulndri <defiaulandari18@gmail.com>
2025-07-22 03:05:24 +0700

    hamdallah yallah

---
+++ src/app/Http/Middleware/ClientAuth.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+// use App\Models\Client;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\User;
+
+class ClientAuth
+{
+ /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $apiKey = $request->header('X-API-KEY');
+
+        if (!$apiKey) {
+            return response()->json(['error' => 'API key is missing'], 401);
+        }
+
+        $user = User::where('api_key', $apiKey)->first();
+
+        if (!$user) {
+            return response()->json(['error' => 'Invalid API key'], 401);
+        }
+
+        return $next($request);
+    }
+}
